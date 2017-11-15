bbdf09a418da943d5b9d6ce7a9e6a3354da5abd6
dirtybits <dirtybitsofficial@gmail.com>
2017-11-15 16:22:37 -0600

    - Added MouseInputHandler - Changed key control input to AWDS - Fixed some typos

---
+++ client/src/main/java/com/survival/sim/common/entities/TreeEntity.java
@@ -0,0 +1,8 @@
+package com.survival.sim.common.entities;
+
+public class TreeEntity extends Entity{
+
+    public TreeEntity(int x, int y, int plane, boolean walkable, int health) {
+        super(x, y, plane, walkable, health);
+    }
+}
