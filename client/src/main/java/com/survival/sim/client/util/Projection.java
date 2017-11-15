fad1a72b8e91f477281e6eb9c8db849fcf57a3fc
dirtybits <dirtybitsofficial@gmail.com>
2017-11-15 16:16:52 -0600

    - Added MouseInputHandler - Changed key control input to AWDS - Fixed some typos

--- client/src/main/java/com/survival/sim/client/util/Projection.java
+++ client/src/main/java/com/survival/sim/client/util/Projection.java
@@ -1,7 +1,6 @@
 package com.survival.sim.client.util;
 
 import com.survival.sim.client.game.Camera;
-import com.survival.sim.client.game.Screen;
 import com.survival.sim.common.entities.Tile;
 import com.survival.sim.common.entities.interfaces.Locatable;
 
@@ -16,7 +15,7 @@ public class Projection {
         int xDif = location.getX() - cameraOffset.getX();
         int yDif = location.getY() - cameraOffset.getY();
 
-        return new Point(xDif * Screen.getTileSize(), yDif * Screen.getTileSize());
+        return new Point(xDif * 64, yDif * 64);
     }
 
     public static Tile screenToWorld(Point point){
